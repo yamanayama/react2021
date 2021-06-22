8f7af26b88d1ff85f26ec988af19c86c522ef68a
yamanayama <ymmtnomail@gmail.com>
2021-06-23 03:13:57 +0900

    added tab and drawer components

--- src/pages/reactSample.js
+++ src/pages/reactSample.js
@@ -2,38 +2,35 @@
 import React, { useState } from "react";
 import styled from '@emotion/styled';
 import Layout from "../components/layout.js"
-import Lottie from "react-lottie";
-import heroAnimation from "../animation/heroillust.json";
+import { AnimationHuman }  from "../components/animationHuman.js"
 import { color, contentWidth, typography } from '../styles/constans'
 import Modal from "../components/modal.js"
+import Tab from "../components/tab.js"
 
 
 const ReactSample = () => {
-    const heroillust = {
-        loop: true,
-        autoplay: true,
-        heroAnimation,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
-        }
-      };
-
     const [showModal, setShowModal] = useState(false);
 
     const openModal = () => {
-    setShowModal(true);
+      setShowModal(true);
     };
 
     return (
         <>
           <Layout>
-            <LottieWrap>
-                <Lottie options={heroillust} />
-            </LottieWrap>
+            <AnimationHuman />
             <Wrap>
-                <H2>modal</H2>
+                <H2>Drawer</H2>
+                <p>スマホサイズで右上からどうぞ！</p>
+            </Wrap>
+            <Wrap>
+                <H2>Modal</H2>
                 <button onClick={openModal}>modal open</button>
                 <Modal showFlag={showModal} setShowModal={setShowModal}title="タイトルよ" discription="つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）"/>
+            </Wrap>
+            <Wrap>
+                <H2>Tab</H2>
+                <Tab/>
             </Wrap>
           </Layout>
         </>
@@ -57,6 +54,7 @@ const LottieWrap = styled.div`
 const Wrap = styled.div`
   margin: auto;
   max-width: ${contentWidth};
+  width: 100%;
   padding: 48px 0;
   text-align: center;
 `;
